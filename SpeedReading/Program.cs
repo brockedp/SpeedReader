9d3651d8294b11fdf3527e63f3cb33914c36d47b
Deno <denobrockert@yahoo.com>
2019-08-12 09:15:26 -0400

    First push of speed reading program

---
+++ SpeedReading/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+
+namespace SpeedReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string[] pathSubstring = path.Split('\\');
+            int binIndex = Array.IndexOf(pathSubstring, "bin");
+            binIndex--;
+            string filePath = "";
+            for (int i = 0; i <= binIndex; i++)
+            {
+                filePath += pathSubstring[i] + "\\";
+            }
+
+            string textFile = "TextFile1.txt";
+            filePath = filePath + textFile;
+
+            Console.WriteLine("Welcome to Speed Reading!");
+            Console.WriteLine();
+            int words = Validator.GetIntWithinRange("Please enter how many words at a time you want to read(1-6): ", 1, 6);
+            double time = Validator.GetDoubleWithinRange("Please enter how quickly you want the words to scan:(.25-2) ", .25, 2);
+            int milliTime = (int)(time * 1000);
+            Console.Clear();
+
+            ReadFileChoice(filePath, words, milliTime);
+
+        }
+        
+        public static void ReadFile(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                if (line == "")
+                {
+                    line = reader.ReadLine();
+                }
+                else
+                {
+                    string[] items = line.Split(' ');
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item);
+                        System.Threading.Thread.Sleep(250);
+                        Console.Clear();
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        public static void ReadFile2Word(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                if (line == "")
+                {
+                    line = reader.ReadLine();
+                }
+                else
+                {
+                    string[] items = line.Split(' ');
+                    for(int i = 0; i< items.Length-1;i+=2)
+                    {
+                        Console.WriteLine($"{items[i]} {items[i+1]}");
+                        System.Threading.Thread.Sleep(250);
+                        Console.Clear();
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        public static void ReadFileChoice(string filePath, int count, int time)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                if (line == "")
+                {
+                    line = reader.ReadLine();
+                }
+                else
+                {
+                    string[] items = line.Split(' ');
+                    for (int i = 0; i < items.Length -(count-1); i += count)
+                    {
+                        for (int j = 0; j < count; j++)
+                        {
+                            //Console.WriteLine();
+                            Console.Write($"{items[i+j]} ");
+
+                        }
+                        System.Threading.Thread.Sleep(time);
+                        Console.Clear();
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+    }
+}
---
+++ SpeedReading/Validator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedReading
+
+{
+    public class Validator
+    {
+        public static int GetInt(string prompt)
+        {
+            string input = GetUserInput(prompt);
+            bool isValid = int.TryParse(input, out int result);
+            if (isValid)
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Please try again");
+                return GetInt(prompt);
+            }
+        }
+
+        public static int GetIntWithinRange(string prompt, int min, int max)
+        {
+            int result = GetInt(prompt);
+            if (result >= min && result <= max)
+            {
+                return result;
+            }
+            else
+            {
+                return GetInt($"Please enter a number between {min} and {max}");
+            }
+        }
+        public static double GetDouble(string prompt)
+        {
+            bool isValid = double.TryParse(prompt, out double result);
+            if (isValid)
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Please try again");
+                return GetDouble(Console.ReadLine());
+            }
+        }
+        public static double GetDoubleWithinRange(string prompt, double min, double max)
+        {
+            string input = GetUserInput(prompt);
+            double result = GetDouble(input);
+            if (result >= min && result <= max)
+            {
+                return result;
+            }
+            else
+            {
+                return GetDouble($"Please enter a number between {min} and {max}");
+            }
+        }
+        public static string GetRequiredString(string message)
+        {
+            string IsString = " ";
+            bool isValid = string.IsNullOrWhiteSpace(IsString);
+            while (isValid)
+            {
+                Console.WriteLine(message);
+                IsString = Console.ReadLine();
+                isValid = string.IsNullOrWhiteSpace(IsString);
+            }
+
+            return IsString;
+        }
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+
+        }
+    }
+}
